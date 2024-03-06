3bfde53a5effeee751fa5a42d76f3574f24f2a5b
raulgonzalez <raulgonji@gmail.com>
2024-03-06 17:01:33 +0100

    Initial commit

---
+++ sample.js
@@ -0,0 +1,15 @@
+// Export function evenOrodd(p = 0) {
+//   return p % 2 ? "od [impar]" : "Even [Par]";
+// }
+// export function evenOrOdd(p = 0) {
+//   if (p !== typeof "number") {
+//     return "invalid type";
+//   }
+
+//   return p % 2 ? "odd[Impar]" : "even [par]";
+// }
+
+// export const arrayLength = (array) => {
+//   const r = 0;
+//   return r;
+// };
---
+++ sample.test.js
@@ -0,0 +1,26 @@
+// Import { arrayLength, evenOrOdd } from "./sample.js";
+
+// Describe("evenOrOdd", () => {
+//   test('If given parameter p is "Hola" it should return "invalid type" ', () => {
+//     const x = "Hola";
+//     const expected = "invalid type";
+//     const r = evenOrOdd(x);
+//     expect(r).toBe(expected);
+//   });
+// });
+
+// test('If given parameter p is "Hola" it should return "invalid type" ', () => {
+//   const x = "Hola";
+//   const expected = "invalid type";
+//   const r = evenOrOdd(x);
+//   expect(r).toBe(expected);
+// });
+
+// describe.only("arrayLength", () => {
+//   test("should be  ", () => {
+//     const x = [];
+//     const expected = 0;
+//     const r = arrayLength(x);
+//     expect(r).toBe(expected);
+//   });
+// });
