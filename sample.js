5bb40cfdca15cd5e98df3c73939c7809986629c3
raulgonzalez <raulgonji@gmail.com>
2024-03-06 20:27:47 +0100

    Add sample test and sample fuctions

--- sample.js
+++ sample.js
@@ -1,15 +1,45 @@
-// Export function evenOrodd(p = 0) {
-//   return p % 2 ? "od [impar]" : "Even [Par]";
-// }
-// export function evenOrOdd(p = 0) {
-//   if (p !== typeof "number") {
-//     return "invalid type";
-//   }
+export const manualArrayLength = (array) => {
+  let r = 0;
 
-//   return p % 2 ? "odd[Impar]" : "even [par]";
-// }
+  // eslint-disable-next-line no-unused-vars
+  for (const iterator of array) {
+    r++;
+  }
 
-// export const arrayLength = (array) => {
-//   const r = 0;
-//   return r;
-// };
+  return r;
+};
+
+export function arrayPush(array, ...elements) {
+  const arrlength = manualArrayLength(array);
+  const elementLength = manualArrayLength(elements);
+  for (let i = 0; i < elementLength; i++) {
+    array[arrlength + i] = elements[i];
+  }
+
+  const newlength = manualArrayLength(array);
+  return newlength;
+}
+
+// Console.log(arrayPush([], 2));
+
+export const fakepop = (array) => {
+  const arr = array[array.length - 1];
+  // eslint-disable-next-line operator-assignment
+  array.length = array.length - 1;
+  return arr;
+};
+
+console.log(fakepop([1, 2, 24]));
+
+export const indexOF = (arr, arrayElement) => {
+  let elemtIndex = -1;
+  const arrayLength = manualArrayLength(arr);
+  for (let i = 0; i < arrayLength; i++)
+    if (arrayElement === arr[i]) {
+      elemtIndex = i;
+    }
+
+  return elemtIndex;
+};
+
+// Console.log(indexOF([1, 2, 3, 7, 35], 35));
